5d71514629cd8f81704477c3b227ec6c8191a850
Russell Greene <russellgreene8@gmail.com>
2023-07-23 16:05:24 -0600

    fmt and a new test

--- tests/cmdline.rs
+++ tests/cmdline.rs
@@ -1,6 +1,6 @@
 use std::{
     env::{consts::EXE_SUFFIX, current_exe, temp_dir},
-    path::PathBuf,
+    path::{Path, PathBuf},
     process::Command,
     thread::sleep,
     time::{Duration, Instant},
@@ -24,12 +24,11 @@ fn wl_screenrec() -> PathBuf {
 }
 
 #[test]
-#[ignore] // not passing yet
-fn audio_history_clip_length() {
+fn history_clip_length() {
     let filename = temp_dir().join("ahcl.mp4");
 
     let mut cmd = Command::new(dbg!(wl_screenrec()))
-        .arg("--audio")
+        .arg("--no-damage")
         .arg("--history")
         .arg("2")
         .arg("-f")
@@ -50,6 +49,40 @@ fn audio_history_clip_length() {
     cmd.wait().unwrap();
     assert!(wait_start.elapsed() < Duration::from_secs(1));
 
+    let dur = file_duration(filename);
+
+    // duration *should* be ~8 (2 seconds of history + 6 seconds after USER1)
+    assert!(dur > Duration::from_secs(8), "{:?} < 8s", dur);
+    assert!(dur < Duration::from_secs_f64(8.5), "{:?} > 8.5s", dur);
+}
+
+#[test]
+fn basic() {
+    let filename = temp_dir().join("basic.mp4");
+
+    let mut cmd = Command::new(dbg!(wl_screenrec()))
+        .arg("--no-damage")
+        .arg("-f")
+        .arg(&filename)
+        .spawn()
+        .unwrap();
+    let pid = Pid::from_raw(cmd.id() as i32);
+
+    sleep(Duration::from_secs(3));
+
+    kill(pid, SIGINT).unwrap();
+
+    let wait_start = Instant::now();
+    cmd.wait().unwrap();
+    assert!(wait_start.elapsed() < Duration::from_secs(1));
+
+    let dur = file_duration(filename);
+
+    assert!(dur > Duration::from_secs_f64(2.5), "{:?} < 2.5s", dur);
+    assert!(dur < Duration::from_secs_f64(2.5), "{:?} > 3.5s", dur);
+}
+
+fn file_duration(filename: PathBuf) -> Duration {
     let json: Value = serde_json::from_str(
         &String::from_utf8(
             Command::new("ffprobe")
@@ -65,13 +98,12 @@ fn audio_history_clip_length() {
     )
     .unwrap();
 
-    let dur: f64 = json.pointer("/format/duration")
+    let dur: f64 = json
+        .pointer("/format/duration")
         .unwrap()
         .as_str()
         .unwrap()
         .parse()
         .unwrap();
-
-    assert!(dur > 8., "{} < 8", dur);
-    assert!(dur < 8.5, "{} > 8.5", dur);
+    Duration::from_secs_f64(dur)
 }
