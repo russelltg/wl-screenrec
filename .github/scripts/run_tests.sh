8ddabab533fb3bb24e0338c26d5aa17cc69282d9
Russell Greene <russellgreene8@gmail.com>
2023-08-21 22:05:43 -0600

    more work on err prop, use nightly

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -4,17 +4,20 @@ set -x
 
 code=0
 
-git clone https://github.com/russelltg/libva-x264
-if [ $? -ne 0] ; then code=$? ; fi
+rustup install nightly
+if [ $? -ne 0 ] ; then code=$? ; fi
 
-(cd libva-x264 && cargo build --release)
-if [ $? -ne 0] ; then code=$? ; fi
+git clone https://github.com/russelltg/libva-x264
+if [ $? -ne 0 ] ; then code=$? ; fi
+
+(cd libva-x264 && cargo +nightly build --release)
+if [ $? -ne 0 ] ; then code=$? ; fi
 
 cp libva-x264/target/release/liblibva_x264.so x264_drv_video.so
-if [ $? -ne 0] ; then code=$? ; fi
+if [ $? -ne 0 ] ; then code=$? ; fi
 
 LIBVA_DRIVERS_PATH=`pwd` LIBVA_DRIVER_NAME=x264 cargo test
-if [ $? -ne 0] ; then code=$? ; fi
+if [ $? -ne 0 ] ; then code=$? ; fi
 
 swaymsg exit
 
