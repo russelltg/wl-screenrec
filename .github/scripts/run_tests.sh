35e2a30e925563d18f899c68a0c30f7fa2803b45
Russell Greene <russellgreene8@gmail.com>
2023-08-21 22:12:44 -0600

    add mesa-utils, hopefully fix prop

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -5,21 +5,22 @@ set -x
 code=0
 
 rustup install nightly
-if [ $? -ne 0 ] ; then code=$? ; fi
+if [ $? -ne 0 ] ; then code=1 ; fi
 
 git clone https://github.com/russelltg/libva-x264
-if [ $? -ne 0 ] ; then code=$? ; fi
+if [ $? -ne 0 ] ; then code=1 ; fi
 
 (cd libva-x264 && cargo +nightly build --release)
-if [ $? -ne 0 ] ; then code=$? ; fi
+if [ $? -ne 0 ] ; then code=1 ; fi
 
 cp libva-x264/target/release/liblibva_x264.so x264_drv_video.so
-if [ $? -ne 0 ] ; then code=$? ; fi
+if [ $? -ne 0 ] ; then code=1 ; fi
 
 LIBVA_DRIVERS_PATH=`pwd` LIBVA_DRIVER_NAME=x264 cargo test
-if [ $? -ne 0 ] ; then code=$? ; fi
+if [ $? -ne 0 ] ; then code=1 ; fi
 
-swaymsg exit
 
 echo $code > /tmp/test_exit_code
+
+swaymsg exit
 
