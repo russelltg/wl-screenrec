93972f1085af242234b2ea7413aea00a1c594c20
Russell Greene <russell@shotover.com>
2024-11-13 00:27:54 -0700

    remove unused var

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ use std::env;
 
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(ffmpeg_7_0)");
-    println!("cargo::rustc-check-cfg=cfg(ffmpeg_7_1)");
 
     for (name, value) in env::vars() {
         if name.starts_with("DEP_FFMPEG_") && !value.is_empty() {
